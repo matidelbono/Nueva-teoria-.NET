77139f60983918a57ab3ac05eebaf56334b903f0
Mati <mati.delbono00@gmail.com>
2022-04-05 12:22:57 -0300

    Clases estáticas y sobrecarga de operadores

--- RepasoNuevosConceptos/RepasoNuevosConceptos/Program.cs
+++ RepasoNuevosConceptos/RepasoNuevosConceptos/Program.cs
@@ -61,6 +61,38 @@ namespace RepasoNuevosConceptos
                     Console.WriteLine("Se ha producido un error");
                 }
             }
+            // Clases Estáticas
+            Estatica.Numero = 5;
+            int Aleatorio = Estatica.ObtenerNumeroAlAzar();
+            Console.ReadLine();
+            //Miembros estáticos en clases comunes
+            Dato PrimerDato = new Dato();
+            PrimerDato.Sumar(3);
+            Dato SegundoDato = new Dato();
+            SegundoDato.Sumar(11);
+            Console.ReadLine();
+            // Métodos de Extensión en clases selladas
+            Empleado empleado = new Empleado();
+            bool ElCuitEsValido = empleado.CuitValido();
+            Console.ReadLine();
+            // Sobrecarga de Métodos
+            VectorDeEnteros primero = new VectorDeEnteros();
+            Console.WriteLine("Cargando el primer vector");
+            primero.CargaDelVector();
+            VectorDeEnteros segundo = new VectorDeEnteros();
+            Console.WriteLine("Cargando el segundo vector");
+            segundo.CargaDelVector();
+            Console.WriteLine("imprimiendo el primer vector");
+            primero.ImprimirOperacion();
+            Console.WriteLine("imprimiendo el segundo vector");
+            segundo.ImprimirOperacion();
+            VectorDeEnteros SumaDeVectores = new VectorDeEnteros();
+            SumaDeVectores = primero + segundo;
+            Console.WriteLine("Vector resultante");
+            SumaDeVectores.ImprimirOperacion();
+            Console.ReadKey();
+
         }
-    }
 }
+}
+
